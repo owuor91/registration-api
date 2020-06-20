16b110501e03a7972af335b0e8bc872fafd6cffd
John Owuor <owuor91@gmail.com>
2020-06-20 17:11:32 +0300

    fix and refactor validations

--- resources/student.py
+++ resources/student.py
@@ -8,6 +8,25 @@ from models.student_model import StudentModel
 
 class Student(Resource):
     def post(self):
+        errors = self.validate_post_student()
+        if (len(errors) != 0):
+            return {'error': True, 'errors': errors}, 400
+        request_phone_number = self.get_value('phone_number')
+
+        if StudentModel.find_student_by_phone_number(request_phone_number):
+            return {'message': 'a student with the number {} already exists'.format(request_phone_number)}, 400
+        else:
+            new_student = StudentModel(first_name=self.get_value('first_name'),
+                                       last_name=self.get_value('last_name'),
+                                       email=self.get_value('email'),
+                                       phone_number=self.get_value('phone_number'),
+                                       date_of_birth=datetime.datetime.strptime(self.get_value('date_of_birth'),
+                                                                                '%Y-%m-%d'),
+                                       sex=self.get_value('sex'))
+            new_student.save_to_db()
+            return {'message': 'student registration, successful'}, 201
+
+    def validate_post_student(self):
         errors = dict()
         if not self.get_value('first_name'):
             errors['first_name'] = 'First name is required'
@@ -27,23 +46,7 @@ class Student(Resource):
         if not self.get_value('sex'):
             errors['sex'] = 'sex is required'
 
-        if (len(errors) != 0):
-            return {'error': True, 'errors': errors}, 400
-
-        request_phone_number = self.get_value('phone_number')
-
-        if StudentModel.find_student_by_phone_number(request_phone_number):
-            return {'message': 'a student with the number {} already exists'.format(request_phone_number)}, 400
-        else:
-            new_student = StudentModel(first_name=self.get_value('first_name'),
-                                       last_name=self.get_value('last_name'),
-                                       email=self.get_value('email'),
-                                       phone_number=self.get_value('phone_number'),
-                                       date_of_birth=datetime.datetime.strptime(self.get_value('date_of_birth'),
-                                                                                '%Y-%m-%d'),
-                                       sex=self.get_value('sex'))
-            new_student.save_to_db()
-            return {'message': 'student registration, successful'}, 201
+        return errors
 
     def get(self, student_id=None):
         student = students = None
@@ -74,7 +77,11 @@ class Student(Resource):
             student.email = self.get_value('email')
 
         if self.get_value('phone_number'):
-            student.phone_number = self.get_value('phone_number')
+            if not student.find_student_by_phone_number(self.get_value('phone_number')):
+                student.phone_number = self.get_value('phone_number')
+            else:
+                return {'message': 'a student with the number {} already exists'.format(
+                    self.get_value('phone_number'))}, 400
 
         student.save_to_db()
         return {'message': 'student updated successfully',
