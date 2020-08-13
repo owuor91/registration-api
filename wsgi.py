321315ab470e4cf712edd64d978afbea4336bdfd
John Owuor <owuor91@gmail.com>
2020-08-13 15:27:26 +0300

    update jwt config

--- wsgi.py
+++ wsgi.py
@@ -2,3 +2,7 @@ from app import create_app
 from config import ProductionConfig
 
 app = create_app(ProductionConfig)
+from flask_jwt_extended import JWTManager
+
+app.config['JWT_SECRET_KEY'] = 'reallynicee38y72piebird'
+jwt = JWTManager(app)
